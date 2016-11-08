b000c67a3e2b0bb4494c19f06c8b76fff4e3c5ac
aleksejkvasnikov <aleksejkvasnikov@gmail.com>
2016-11-08 16:59:31 +0700

    Fixed

--- 1/lab1.py
+++ 1/lab1.py
@@ -2,33 +2,32 @@ import requests
 import re
 i=0
 allemails= []
-link = ('http://mosigra.ru')
+link = ('http://www.rbc.ru')
 def rekursia(link,i):
 	global allemails
-	if i < 2: ##глубина 3
-		#получили страницу
+	if i < 2: 
+
 		response = requests.get(link)
-		#собрали все ссылки	
-		links = re.findall('"(http://www.mosigra.*?)"', response.text)
-		## собрали всю почту
+
+		links = re.findall('"(http://www.rbc.*?)"', response.text)
+
 		for link in links:
-		# выводим почту
+
 			print(link)			
-		emails = re.findall(r'[\w\.-]+@[\w\.-]+', response.text) 
-		#убрали повторения
+		emails = re.findall(r'[\w\.-]+@[\w\.-]+\.+[\w\.-]+', response.text) 
+
 		links = set(links)
-		#шаг в глубину	
+
 		i=i+1		
-		#сохранили			
+	
 		allemails =  allemails + emails
-		#для каждой почты		
+	
 		for link in links:
-			# рекурсивный вызов
+
 			rekursia(link,i)
 	else: return		
 rekursia(link, i)				
 allemails = set(allemails)
 for allemail in allemails:
-	# выводим почту
-	print(allemail)
 
+	print(allemail)
